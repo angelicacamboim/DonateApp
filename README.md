5f91c949d58fff4ced9fa85d94be8e26891af98d
Angelica Bayer <angelicacamboim@gmail.com>
2020-07-25 20:52:44 -0300

    backend.4

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DonateApp
 MaratonaDEV 3.0
+
+
+<img src="/img/screencaptureIndex.pn" style=" width:100px ; height:100px " />
--- server.js
+++ server.js
@@ -7,16 +7,51 @@ const server = express() //funcionalidade
 //configurar o servidor para apresentar arquivos estáticos
 server.use(express.static('public'))
 
+//habilitar body do formulário
+server.use(express.urlencoded({extended: true}))
 
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-    express: server
+    express: server,
+    noCache: true
 })
+
+//lista de doadores: vetor ou array
+const donors = [
+    {
+        name: "Diego Fernandes",
+        blood: "AB+"
+    },
+    {
+        name: "Nimue",
+        blood: "A+"
+    },
+    {
+        name: "Angel",
+        blood: "O-"
+    }
+]
 
 //configurar a apresentação da página
 server.get('/', function(req, res){
-    return res.render("index.html")
+    return res.render("index.html", { donors})
+})
+
+server.post('/', function(req, res){
+//pegar dados do formulario
+const name = req.body.name
+const email = req.body.email
+const blood = req.body.blood
+
+//coloco valores dentro do array
+donors.push({
+    name: name,
+    blood: blood
+})
+
+return res.redirect("/")
+
 })
 
 //ligar o servidor e permitir o acesso na porta 3000
